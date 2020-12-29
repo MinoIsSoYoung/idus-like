3af4ab8d8cc249335bd3ba1ae27d9f2c2fd6420e
soyoung <so_young_jung@naver.com>
2020-12-29 17:43:17 +0900

    스프링부트 프로젝트 생성

---
+++ server/src/main/java/com/minosoyoung/iduslike/IdusLikeApplication.java
@@ -0,0 +1,13 @@
+package com.minosoyoung.iduslike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdusLikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdusLikeApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/minosoyoung/iduslike/IdusLikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minosoyoung.iduslike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdusLikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
