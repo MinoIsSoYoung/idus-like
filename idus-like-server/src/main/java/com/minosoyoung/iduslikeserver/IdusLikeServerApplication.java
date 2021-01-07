96f96131db28666858ba6506d234314a24f8a73e
soyoung <so_young_jung@naver.com>
2021-01-07 18:08:36 +0900

    ENV: 스프링 프로젝트 생성

---
+++ idus-like-server/src/main/java/com/minosoyoung/iduslikeserver/IdusLikeServerApplication.java
@@ -0,0 +1,13 @@
+package com.minosoyoung.iduslikeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdusLikeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdusLikeServerApplication.class, args);
+	}
+
+}
---
+++ idus-like-server/src/test/java/com/minosoyoung/iduslikeserver/IdusLikeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minosoyoung.iduslikeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdusLikeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
